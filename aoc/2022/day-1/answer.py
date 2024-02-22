72a9e2e2b56d2a4cadc08029000ceea9eba532c3
Anupa Perera <anupa@waan.email>
2024-02-22 17:23:11 +0530

    Add | Dockerfile and devcontainer configuration

--- aoc/2022/day-1/answer.py
+++ aoc/2022/day-1/answer.py
@@ -1,35 +1,20 @@
-from dotenv import load_dotenv
-import os
 import itertools
 import heapq
-
+import os
 
 class CalorieCounter:
-    def __init__(self) -> None:
-        load_dotenv()
-        self.project_folder = os.getenv('AOC_2022_ROOT')
-
-        if self.project_folder is None:
-            raise ValueError("Environment variable AOC_2022_ROOT not found")
-
-        self.file_path = os.path.join(
-            self.project_folder, 'day-1/puzzle-input.txt')
-
     def read_and_process(self) -> None:
-        try:
-            with open(self.file_path, 'r') as file:
-                calories = [int(line) if line.strip() else '' for line in file]
-
+        file_path = './puzzle-input.txt'
+        
+        if not os.path.exists(file_path):
+            print(f"File not found: {file_path}")
+            return 
+        
+        with open(file_path, 'r') as file:
+            calories = [int(line) if line.strip() else '' for line in file]
+                
             self.calories_sum = [sum(group) for is_empty, group in itertools.groupby(
                 calories, lambda x: x == '') if not is_empty]
-
-        except FileNotFoundError:
-            print(f"File not found: {self.file_path}")
-            return None
-
-        except Exception as e:
-            print(f"An error occurred: {e}")
-            return None
 
     def max_group_sum(self) -> int:
         return max(self.calories_sum) if self.calories_sum else None
@@ -37,9 +22,13 @@ class CalorieCounter:
     def sum_of_largest_three(self) -> int:
         return sum(heapq.nlargest(3, self.calories_sum)) if self.calories_sum else None
 
-
+# Example usage
 counter = CalorieCounter()
 counter.read_and_process()
 
-print(counter.max_group_sum())  # 1st answer
-print(counter.sum_of_largest_three())  # 2nd answer
+if hasattr(counter, 'calories_sum'):
+    print(counter.max_group_sum())  # 1st answer
+    print(counter.sum_of_largest_three())  # 2nd answer
+    
+else:
+    print("File read error.")
