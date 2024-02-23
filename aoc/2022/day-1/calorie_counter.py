aeee49693587195dc1cda0da139004aca35d4c68
Anupa Perera <anupa@waan.email>
2024-02-23 10:27:57 +0530

    Update | Move to README

--- aoc/2022/day-1/calorie_counter.py
+++ aoc/2022/day-1/calorie_counter.py
@@ -8,16 +8,13 @@ class CalorieCounter:
         file_path = './aoc/2022/day-1/puzzle-input.txt'
 
         if not os.path.exists(file_path):
-            print(f"File not found: {file_path}")
-            return
+            raise FileNotFoundError(f"File not found: {file_path}")
 
         with open(file_path, 'r') as file:
             calories = [int(line) if line.strip() else '' for line in file]
 
             self.calories_sum = [sum(group) for is_empty, group in itertools.groupby(
                 calories, lambda x: x == '') if not is_empty]
-
-        return self.calories_sum
 
     def max_group_sum(self) -> int:
         return max(self.calories_sum) if self.calories_sum else None
