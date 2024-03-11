8904022582daea2ec3ed33da9bc7bc5203b3737b
Anupa Perera <anupa@waan.email>
2024-03-11 10:08:19 +0530

    Package update.

--- fabfile.py
+++ fabfile.py
@@ -3,9 +3,7 @@ from fabric2 import task
 
 @task
 def autoflake(ctx):
-    ctx.run(
-        "autoflake --remove-all-unused-imports --recursive --remove-unused-variables --in-place code_challenges"
-    )
+    ctx.run("autoflake --remove-all-unused-imports --recursive --remove-unused-variables --in-place code_challenges")
 
 
 @task
