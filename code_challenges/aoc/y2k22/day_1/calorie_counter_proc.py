b7f4ec3a9b5c2b6ca830ad544a373e291ecb1e29
Sandman <anupa@waan.email>
2024-04-12 14:59:44 +0530

    Wr 48 (#6)
    * Update.
    
    * Update | License file.
    
    * Update | License file.
    
    * Update | License file.
    
    * Update | include MIT License.
    
    * Update.
    
    * Update.
    
    * Update.
    
    * Update | Package config files.
    
    * Update.
    
    * Update.
    
    * Update.
    
    * Update.
    
    * Update.
    
    * Update | header docblock.
    
    * Update | file path setter method.
    
    * Update.
    
    * Update.
    
    * Add | launch.json for debugging.
    
    * Update.
    
    * Update.
    
    * Update.
    
    * Update.
    
    * Add | pyproject.toml file.
    
    * Update.
    
    * Update.
    
    * Update.
    
    * Update.
    
    * Update.
    
    * Update.
    
    * Update.
    
    * Update.
    
    * Update.

--- code_challenges/aoc/y2k22/day_1/calorie_counter_proc.py
+++ code_challenges/aoc/y2k22/day_1/calorie_counter_proc.py
@@ -200,6 +200,8 @@ class CalorieCounter:
             The sum of the three largest calorie groups or None if there are less than three groups.
         """
         return sum(heapq.nlargest(3, self.calories_sum)) if self.calories_sum else None
+
+
 # def test() -> None:
 #     """
 #     This function creates an instance of the CalorieCounter class, reads and processes the calorie
