74bdc8adf3dc7fb8d411aa1a99a9cf8938d4796e
Anupa Perera <anupa@waan.email>
2024-04-16 00:03:28 -0500

    Update | Pytest.

--- code_challenges/aoc/y2k22/day_1/calorie_counter_oop.py
+++ code_challenges/aoc/y2k22/day_1/calorie_counter_oop.py
@@ -14,11 +14,13 @@ Modified: 15/04/2024 by admin@waan.email
 import heapq
 import itertools
 import os
-import cProfile
-import pstats
+
+# import cProfile
+# import pstats
 from typing import Union
-from icecream import ic
-from pympler import asizeof
+
+# from icecream import ic
+# from pympler import asizeof
 
 # from pydantic import BaseModel
 
@@ -190,41 +192,41 @@ class CalorieCounter:
         return sum(heapq.nlargest(3, self.calories_sum)) if self.calories_sum else None
 
 
-def test() -> None:
-    """
-    This function creates an instance of the CalorieCounter class, reads and processes the calorie
-    data, and then prints the maximum sum of calorie groups and the sum of the three largest
-    calorie groups.
-    """
-    try:
-        calorie_counter = CalorieCounter(
-            "./code_challenges/aoc/y2k22/day_1/puzzle-input.txt"
-        )
-        file = calorie_counter.read_calories()
-        calorie_counter.process_calories(file)
+# def test() -> None:
+#     """
+#     This function creates an instance of the CalorieCounter class, reads and processes the calorie
+#     data, and then prints the maximum sum of calorie groups and the sum of the three largest
+#     calorie groups.
+#     """
+#     try:
+#         calorie_counter = CalorieCounter(
+#             "./code_challenges/aoc/y2k22/day_1/puzzle-input.txt"
+#         )
+#         file = calorie_counter.read_calories()
+#         calorie_counter.process_calories(file)
 
-        ic(calorie_counter.puzzle_file_path)
-        ic(calorie_counter.max_group_sum())
-        ic(calorie_counter.sum_of_largest_three())
-        print(asizeof.asized(calorie_counter, detail=1).format())
+#         ic(calorie_counter.puzzle_file_path)
+#         ic(calorie_counter.max_group_sum())
+#         ic(calorie_counter.sum_of_largest_three())
+#         print(asizeof.asized(calorie_counter, detail=1).format())
 
-    except (FileNotFoundError, TypeError, ValueError) as error:
-        print(f"System error: {error}")
+#     except (FileNotFoundError, TypeError, ValueError) as error:
+#         print(f"System error: {error}")
 
 
-if __name__ == "__main__":
-    """
-    If the script is being run directly (not imported as a module), the test function is called.
-    """
-    profiler = cProfile.Profile()
-    profiler.enable()
+# if __name__ == "__main__":
+#     """
+#     If the script is being run directly (not imported as a module), the test function is called.
+#     """
+#     profiler = cProfile.Profile()
+#     profiler.enable()
 
-    test()
+#     test()
 
-    profiler.disable()
-    profiler.print_stats(sort="time")
-    stats = pstats.Stats(profiler).sort_stats("time")
-    stats.dump_stats("my.prof")
+#     profiler.disable()
+#     profiler.print_stats(sort="time")
+#     stats = pstats.Stats(profiler).sort_stats("time")
+#     stats.dump_stats("my.prof")
 
 """
 ./code_challenges/aoc/y2k22/day_1/calorie_counter.py
