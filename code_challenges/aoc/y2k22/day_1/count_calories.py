93441d8f6040e98ac397915fb26efc9e050e88c5
Sandman <anupa@waan.email>
2024-05-03 23:03:28 +0530

    Update. (#53)

--- code_challenges/aoc/y2k22/day_1/count_calories.py
+++ code_challenges/aoc/y2k22/day_1/count_calories.py
@@ -8,7 +8,7 @@ puzzle_file_path: str = "./puzzle-input.txt"
 with open(puzzle_file_path, "r", encoding="utf-8") as file:
     calories_list = [int(line) if line.strip() else "" for line in file]
 
-calories_sum: list[int | str] = []
+calories_sum: list[int] = []
 calories_group_sum: int = 0
 
 for calories in calories_list:
@@ -18,7 +18,8 @@ for calories in calories_list:
     else:
         if calories_group_sum != 0:
             calories_sum.append(calories_group_sum)
-            calories_group_sum: int = 0
+
+        calories_group_sum: int = 0
 
 if calories_group_sum != 0:
     calories_sum.append(calories_group_sum)
